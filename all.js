148a9381de043003e871e1e977113bd0586e07e0
liyian <liyian1124@gmail.com>
2025-11-10 00:07:23 +0800

    使用axios、validate、c3等js套件，完成任務六、七

--- all.js
+++ all.js
@@ -1,47 +1,28 @@
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
-
-const ticketCards = document.querySelector(".ticketCard-area");
-const regionSearch = document.querySelector(".regionSearch");
-const searchResult = document.querySelector("#searchResult-text");
+let data = [];
+axios
+  .get(
+    "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json"
+  )
+  .then(function (res) {
+    data = res.data.data;
+    console.log(data);
+    //啟動
+    init();
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 // 用函式產生單一卡片 HTML
-function createTicketCard({imgUrl, area, rate, name, description, group, price}) {
+function createTicketCard({
+  imgUrl,
+  area,
+  rate,
+  name,
+  description,
+  group,
+  price,
+}) {
   return `
     <li class="ticketCard">
       <div class="ticketCard-img">
@@ -73,97 +54,180 @@ function createTicketCard({imgUrl, area, rate, name, description, group, price})
 }
 
 // 渲染畫面函式，可依地區篩選
+const ticketCards = document.querySelector(".ticketCard-area");
+const regionSearch = document.querySelector(".regionSearch");
+const searchResult = document.querySelector("#searchResult-text");
+
+//渲染函式
 function renderCards(filterArea = "") {
-  const filteredData = filterArea ? data.filter(item => item.area === filterArea) : data;
+  const filteredData = filterArea
+    ? data.filter((item) => item.area === filterArea)
+    : data;
   ticketCards.innerHTML = filteredData.map(createTicketCard).join("");
 
   regionSearch.value = filterArea;
+  //顯示搜尋筆數
   searchResult.textContent = `本次搜尋共 ${filteredData.length} 筆資料`;
+  //顯示圓餅圖
+  showchart(filteredData);
 }
+// 下拉選單監聽事件
+regionSearch.addEventListener("change", (e) => {
+  renderCards(e.target.value);
+});
 
 // 初始化畫面
 function init() {
   renderCards();
 }
 
-// 下拉選單監聽事件
-regionSearch.addEventListener("change", e => {
-  renderCards(e.target.value);
-});
+//將輸入表單資料檢核並加入data
+const form = document.querySelector(".addTicket-form");
+//檢核條件
+const constraints = {
+  imgUrl: {
+    url: {
+      schemes: ["http", "https"],
+      message: "網址格式錯誤",
+    },
+  },
+  area: {
+    presence: { allowEmpty: false, message: "必填" },
+  },
+  price: {
+    numericality: {
+      onlyInteger: true,
+      greaterThan: 0,
+      message: "必須是正整數",
+    },
+  },
+  name: {
+    presence: { allowEmpty: false, message: "必填" },
+  },
+  description: {
+    presence: { allowEmpty: false, message: "必填" },
+  },
+  rate: {
+    numericality: {
+      onlyInteger: true,
+      greaterThanOrEqualTo: 0,
+      lessThanOrEqualTo: 10,
+      message: "需介於 0 ~ 10",
+    },
+  },
+  group: {
+    numericality: {
+      onlyInteger: true,
+      greaterThan: 0,
+      message: "必須是正整數",
+    },
+  },
+};
 
-// 啟動
-init();
+// 顯示錯誤訊息
+function showErrorMessage(errors) {
+  Object.entries(errors).forEach(([key, value]) => {
+    const alertMessage = document.querySelector(`[data-message="${key}"]`);
+    alertMessage.innerHTML = `
+        <i class="fas fa-exclamation-circle"></i>
+        <span>${value.join("")}</span>`;
+  });
+}
 
-//將輸入表單資料加入data
-const form = document.querySelector(".addTicket-form"); 
+// 清除錯誤顯示
+function clearErrorMessage() {
+  const arr = [
+    "imgUrl",
+    "area",
+    "price",
+    "name",
+    "description",
+    "rate",
+    "group",
+  ];
+  arr.forEach((item) => {
+    const alertMessage = document.querySelector(`[data-message="${item}"]`);
+    alertMessage.innerHTML = "";
+  });
+}
 
-//逐一檢核輸入欄位(未做完)
-// form.addEventListener("focusout", e => {
-//   if (e.target.matches("input, select, textarea")) {
-//     validateField(e.target)
-//   }
-// });
-
-//submit後轉換key值加入data並重新渲染
-form.addEventListener("submit", e => {
+//submit後value文字轉數字，經檢核後加入data並重新渲染
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  //檢核所有輸入欄位(未做完)
   const allFields = form.querySelectorAll("input, select, textarea");
-  // let isValid = true;
-  // allFields.forEach(field => {
-  //   if(!validateField(field)){
-  //     isValid = false;
-  //   }
-  // })
-
-  
+  //將輸入的所有值，經文字轉數字整理成待放入data的物件
   const formData = new FormData(form);
+  const ticket = Object.fromEntries(
+    formData
+      .entries()
+      .map(([key, value]) => [
+        key,
+        value != "" && !isNaN(value) ? Number(value) : value.trim(),
+      ])
+  );
+  //檢核所有輸入的值
+  const errors = validate(ticket, constraints, { fullMessages: false });
+  // 清除之前的錯誤訊息
+  clearErrorMessage();
 
-  //key配對表
-  const keyMap = {
-  "圖片網址": "imgUrl",
-  "景點地區": "area",
-  "套票金額": "price",
-  "套票名稱": "name",
-  "套票描述": "description",
-  "套票星級": "rate",
-  "套票組數": "group"
-  };
-  
-
-  //建立新增加的一筆套票物件
-  //方法一
-  const addData = Object.fromEntries(formData.entries().map(
-    ([key, value]) => [keyMap[key] || key, Number(value) ? Number(value) : value.trim()]
-  ));
-  
-  //方法二
-  // const addData = formData.entries().reduce((acc, [key, value]) => {
-  //   const newkey = keyMap[key] || key;
-  //   acc[newkey] = Number(value) ? Number(value) : value.trim();
-  //   return acc
-  // }, {});
-
-  //將新增的資料套件加入data
-  data.push({id: data.length, ...addData});
-
-  //重新渲染
-  renderCards();
-
-  //清空輸入資料
-  allFields.forEach((el) => {
-    if(el.type !== "submit"){
-      el.value = "";
-    }
-  });
+  if (errors) {
+    // 顯示錯誤訊息
+    showErrorMessage(errors);
+  } else {
+    //放入data
+    data.push({ id: data.length, ...ticket });
+    //重新渲染
+    renderCards();
+    // 清空輸入框
+    allFields.forEach((el) =>
+      el.type !== "submit" ? (el.value = "") : (el.value = value)
+    );
+  }
 });
 
-//檢核輸入資料(未做完)
-// function validateField(field){
-// }
+//顯示圓餅圖的函式
+function showchart(filteredData) {
+  // 篩選地區，並累加數字上去
+  // totalObj 會變成 {高雄: 2, 台北: 1, 台中: 2}
+  let totalObj = {};
+  filteredData.forEach(function (item, index) {
+    if (totalObj[item.area] == undefined) {
+      totalObj[item.area] = 1;
+    } else {
+      totalObj[item.area] += 1;
+    }
+  });
 
-// 綠島自由行套裝行程
-// https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true
-// 嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合。
-// 1400
+  // newData = [["高雄", 2], ["台北",1], ["台中", 1]]
+  let newData = [];
+  let area = Object.keys(totalObj);
+  // area output ["高雄","台北","台中"]
+  area.forEach(function (item, index) {
+    let ary = [];
+    ary.push(item);
+    ary.push(totalObj[item]);
+    newData.push(ary);
+  });
+
+  // 將 newData 丟入 c3 產生器
+  const chart = c3.generate({
+    bindto: "#chart",
+    data: {
+      columns: newData,
+      type: "donut",
+      colors: {
+        高雄: "#E68618",
+        台中: "#5151D3",
+        台北: "#26C0C7",
+      },
+    },
+    donut: {
+      title: "套票地區比重",
+      width: 10,
+      label: {
+        show: false,
+      },
+    },
+  });
+}
